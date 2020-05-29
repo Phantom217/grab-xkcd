021cb44622205cb1df2cffeca7833613d4441c51
Tyler Oalman <tyleroalman@gmail.com>
2020-05-29 16:38:54 -0500

    Implement Comic

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,5 @@
+use std::convert::TryFrom;
+
 use anyhow::Result;
 use serde_derive::Deserialize;
 
@@ -19,6 +21,27 @@ impl XkcdClient {
 }
 
 #[allow(dead_code)]
+pub struct Comic {
+    title: String,
+    num: usize,
+    date: String,
+    desc: String,
+    img_url: String,
+}
+
+impl From<ComicResponse> for Comic {
+    fn from(cr: ComicResponse) -> Self {
+        Self {
+            title: cr.title,
+            num: cr.num,
+            date: format!("{}-{}-{}", cr.day, cr.month, cr.year),
+            desc: cr.alt,
+            img_url: cr.img,
+        }
+    }
+}
+
+#[allow(dead_code)]
 #[derive(Deserialize)]
 pub struct ComicResponse {
     month: String,
@@ -32,4 +55,12 @@ pub struct ComicResponse {
     img: String,
     title: String,
     day: String,
+}
+
+impl TryFrom<String> for ComicResponse {
+    type Error = anyhow::Error;
+
+    fn try_from(json: String) -> Result<Self, Self::Error> {
+        serde_json::from_str(&json).map_err(|e| e.into())
+    }
 }
